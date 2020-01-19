c4f87d679ea1ede5cb54eb0d060a9259637ff3cf
ashr81 <ashrithreddi81@gmail.com>
2020-01-20 00:10:17 +0530

    Adds backend api URI

--- src/utils/request.tsx
+++ src/utils/request.tsx
@@ -4,7 +4,7 @@ import { RequestOptions } from '../react-app-env';
 const request = (options: RequestOptions) => {
   const { headers, ...restOptions } = options;
   const client = axios.create({
-    baseURL: process.env.REACT_APP_API_URL,
+    baseURL: process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'http://ashrith-ea-backend.herokuapp.com',
     headers: {
       accept: 'application/json',
       ...headers
