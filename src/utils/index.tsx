bfcf5e7f852040c1d2ddb2f0fa0b705bb5607e0d
ashr81 <ashrithreddi81@gmail.com>
2020-01-20 00:06:24 +0530

    removed unnecessary console.log

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -1,7 +1,6 @@
 export const debounce = (func: any, delay: number) => {
   var timeoutId: number;
   return () => {
-    console.info(timeoutId)
     window.clearTimeout(timeoutId)
     timeoutId = window.setTimeout(func, delay)
   }
