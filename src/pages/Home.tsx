5f6f3494253563a9020e9d52bd3902b207278f94
ashr81 <ashrithreddi81@gmail.com>
2020-01-19 23:41:11 +0530

    Adds contact us page link from home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Flex, Text } from '../components/atoms';
+import { useHistory } from 'react-router-dom';
+import { CONTACT_US } from '../routes/public';
 
 const HomePage = () => {
+  const { push } = useHistory()
   return (
-    <div>Home page</div>
+    <Flex width='100vw' height='100vh' justifyContent='center' alignItems='center'>
+      <Text as='h4' fontSize='xl' cursor='pointer' onClick={useCallback(() => push(CONTACT_US), [])}>Browser to Contact page</Text>
+    </Flex>
   )
 }
 
